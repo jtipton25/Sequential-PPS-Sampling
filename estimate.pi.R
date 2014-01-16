58f77e20c54061755d3442e51e006f516d0493ac
JTipton25 <jtipton25@gmail.com>
2014-01-16 12:06:21 -0700

    Cleaned up and added to sequential.pps

--- estimate.pi.R
+++ estimate.pi.R
@@ -20,46 +20,47 @@ make.prob.perm <- function(N){
   apply(pi, 2, mean)
 }
 
-##
-## Analytic Formula for sampling weights
-##
-
-##
-## \hat{\pi}_i - \pi_i  = (\frac{(N - 2)!} {N!} * \sum_{k = 1}^{N - 1} k \frac{k} {k + 1} + \frac{1} {2} - \frac{N - 1} {N}) * (N - i)
-##
+make.HN <- function(N){ # calculate the harmonic number H_n <- note this is an approximation
+	-digamma(1) + digamma(N+1)
+}
 
 make.pi <- function(N){
 	if(floor(N) < N){
 		"N must be an integer"
 	} else if(N < 2){
-  	"N must be greater than 2"
+		"N must be greater than 2"
 	} else if(N == 2){
-    	bias <- c(1 / 4, 0)
-    } else {
-  	bias <- vector(length = N)
-  	for(j in 1:(N-1)){
-  		#bias[j] <- (factorial(N - 2) / factorial(N) * (sum(1:(N-1) * 1:(N-1) / (2:N))) + 1 /2) #* (N - j)
-  		#bias[j] <- (factorial(N - 2) / factorial(N) * (sum(1:(N-1) * 1:(N-1) / (2:N))) + 1 /2 - (N - 1) / N) * (N - j)
-  		bias[j] <- (1 / (N * (N - 1)) * (sum(1:(N-1) * 1:(N-1) / (2:N))) + 1 /2 - (N - 1) / N) * (N - j)
-  	}
-  	bias[N] <- 0
-  	return(bias)
-  }
+		bias <- c(1 / 4, 0)
+	} else {
+		HN <- make.HN(N)
+		bias <- vector(length = N)
+		for(j in 1:(N-1)){
+			#bias[j] <- (factorial(N - 2) / factorial(N) * (sum(1:(N-1) * 1:(N-1) / (2:N))) + 1 /2) #* (N - j)
+			#bias[j] <- (factorial(N - 2) / factorial(N) * (sum(1:(N-1) * 1:(N-1) / (2:N))) + 1 /2 - (N - 1) / N) * (N - j)
+			#bias[j] <- (1 / (N * (N - 1)) * (sum(1:(N-1) * 1:(N-1) / (2:N))) + 1 /2 - (N - 1) / N) * (N - j) #This works well
+			bias[j] <- (HN - 1) / (N * (N - 1)) * (N - j)
+		}
+		bias[N] <- 0
+		return(bias)
+	}
 }
+
+##
+## Analytic Formula for sampling weights
+##
+
+##
+## \hat{\pi}_i - \pi_i  = \frac{H_N - 1} {N (N - 1)} * (N - i)
+##
 
 
 
 ## Works for N=1:8 relatively quick, slows down dramatically for N > 8
 N <- 4
 ## Works fast
-make.prob(N) + 1:N / N
+make.pi(N) + 1:N / N
  ## Works slow
 make.prob.perm(N)
 ##
-make.prob(N) + 1:N / N - make.prob.perm(N)
-
-
-
-
-
+make.pi(N) + 1:N / N - make.prob.perm(N)
 
